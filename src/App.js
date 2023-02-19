46e567cf07ca740ff396f4aa7a7fbef44c9df3c3
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-18 22:20:44 -0300

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App(){
+  // This represents the state of this component somehow
+  // In this case, the state is set to be whatever is returned by the API
+  let [starWarsData, setStarWarsData] = React.useState({})
+
+  // This shows that we have an infinity loop *
+  console.log("Component rendered")
+
+  // Fetch information on Star Wars
+  fetch("https://swapi.dev/api/people/")
+        .then(res => res.json())
+        .then(data => setStarWarsData(data))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Transform JSON object into an String and display it. */}
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+// * The infinity loop is duo to how React handles changes on the state of a
+// component. Always a state is changed, React rerender the entire component
+// to reflect the changes. Therefore, always we fetch the API we are triggering
+// React to rerender the component. As that happens a new fetch to the API
+// is triggered, which cause the omponent to be rendered again, and so on.
